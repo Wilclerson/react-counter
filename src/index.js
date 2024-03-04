115c9660f067841e9147d95b12a5416af7dc104e
wraymond2017 <wraymond2017@outlook.com>
2024-03-04 17:11:44 -0500

    Final Commit

--- src/index.js
+++ src/index.js
@@ -2,16 +2,44 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import PropTypes from 'prop-types'; // ES6
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function SimpleCounter(props){
+  return (<div className="bigCounter">
+    <div className="calendar">
+    <FontAwesomeIcon icon={fas.faClock} />
+    </div>
+    <div className="four">{props.digitFour % 10}</div>
+    <div className="three">{props.digitThree % 10}</div>
+    <div className="two">{props.digitTwo % 10}</div>
+    <div className="one">{props.digitOne % 10}</div>
+  </div>);
+}
+
+SimpleCounter.propTypes = {
+  digitFour: PropTypes.number,
+  digitThree: PropTypes.number,
+  digitTwo: PropTypes.number,
+  digitOne: PropTypes.number
+};
+
+let counter = 0;
+
+setInterval(function(){
+  const four = Math.floor(counter/1000);
+  const three = Math.floor(counter/100);
+  const two = Math.floor(counter/10);
+  const one = Math.floor(counter/1);
+  console.log(four, three, two, one);
+
+  counter++;
+
+  root.render(
+      <SimpleCounter digitOne={one} digitTwo={two} digitThree={three} digitFour={four}/>
+  );
+},1000);
